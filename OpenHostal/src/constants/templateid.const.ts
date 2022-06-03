43667123674dd5e6ac798b18a5334c47569ecc6b
WilmaraRuizD <51034298+WilmaraRuizD@users.noreply.github.com>
2022-06-03 17:58:15 -0500

    Delete OpenHostal directory

--- OpenHostal/src/constants/templateid.const.ts
+++
@@ -1,3 +0,0 @@
-export default {
-    'SEND_CODE':'d-ebef0995557749fab593edb5edde0ec3'
-}
--- OpenHostal/src/sql/config.ts
+++
@@ -1,9 +0,0 @@
-import { Pool } from "pg";
-
-export const pool = new Pool({
- user:'postgres',
- host:'localhost',
- password:'12345',
- database:'openhostel',
- port:5432
-});
